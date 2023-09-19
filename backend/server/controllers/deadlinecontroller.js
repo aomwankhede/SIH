e9ecb67355086b15730e2c1cf135eaa4e7f01633
aomwankhede <wankhede.aom@gmail.com>
2023-09-20 04:00:52 +0530

    Backend of submission functionality done!!

--- backend/server/controllers/deadlinecontroller.js
+++ backend/server/controllers/deadlinecontroller.js
@@ -3,14 +3,21 @@ const Deadline = require("../models/deadline");
 exports.createdeadline = async (req, res, next) => {
   try {
     //   const puserid = req.user._id
-    const { sid, pid, marks, description, googleDriveLink, projectname, deadlineDate } =
-      req.body;
+    const {
+      sid,
+      pid,
+      marks,
+      description,
+      googleDriveLink,
+      projectname,
+      deadlineDate,
+    } = req.body;
     const deadlinedata = new Deadline({
       studentid: sid,
       proffid: pid,
       marks,
       description,
-      googleDriveLink:googleDriveLink,
+      googleDriveLink: googleDriveLink,
       projectname: projectname,
       deadlineDate: deadlineDate,
     });
@@ -29,6 +36,30 @@ exports.getdeadlines = async (req, res, next) => {
     const deadlines = await Deadline.find({});
     res.json({
       data: deadlines,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.updatedeadlines = async (req, res, next) => {
+  try {
+    const { projectname, link } = req.body;
+
+    // Find the first document with the specified projectname and empty googleDriveLink
+    const updatedDeadline = await Deadline.findOneAndUpdate(
+      { projectname: projectname, googleDriveLink: "" },
+      { $set: { googleDriveLink: link } },
+      { sort: { createdAt: 1 } } // Sort in ascending order of createdAt to get the first matching document
+    );
+
+    if (!updatedDeadline) {
+      return res.status(404).json({ message: "No matching deadline found" });
+    }
+
+    res.json({
+      message: "Deadline updated successfully",
+      data: updatedDeadline,
     });
   } catch (err) {
     next(err);
--- backend/server/routes/route.js
+++ backend/server/routes/route.js
@@ -1,41 +1,90 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const userController = require('../controllers/usercontrollers');
-const projectController = require('../controllers/projectcontroller');
-const deadlineController = require('../controllers/deadlinecontroller')
-const upload = require('../middlewarre/upload')
-const submit = require('../middlewarre/submit')
- 
-router.post('/signup',upload.array('projectfile[]'), userController.signup);
- 
-router.post('/login', userController.login);
+const userController = require("../controllers/usercontrollers");
+const projectController = require("../controllers/projectcontroller");
+const deadlineController = require("../controllers/deadlinecontroller");
+const upload = require("../middlewarre/upload");
+const submit = require("../middlewarre/submit");
 
-router.get('/dashboard',userController.allowIfLoggedin, projectController.getprojects);
+router.post("/signup", upload.array("projectfile[]"), userController.signup);
 
-router.get('/dashboard/requests',userController.allowIfLoggedin, projectController.getrequests);
+router.post("/login", userController.login);
 
-router.post('/project',userController.allowIfLoggedin, userController.grantAccess('createOwn', 'project'), projectController.projectsave);
+router.get(
+  "/dashboard",
+  userController.allowIfLoggedin,
+  projectController.getprojects
+);
 
-router.put('/project/:projectid',userController.allowIfLoggedin, userController.grantAccess('updateOwn', 'project'), projectController.projectupdate);
+router.get(
+  "/dashboard/requests",
+  userController.allowIfLoggedin,
+  projectController.getrequests
+);
 
-router.delete('/project/:projectid',userController.allowIfLoggedin, userController.grantAccess('deleteOwn', 'project'), projectController.projectdelete);
+router.post(
+  "/project",
+  userController.allowIfLoggedin,
+  userController.grantAccess("createOwn", "project"),
+  projectController.projectsave
+);
+
+router.put(
+  "/project/:projectid",
+  userController.allowIfLoggedin,
+  userController.grantAccess("updateOwn", "project"),
+  projectController.projectupdate
+);
+
+router.delete(
+  "/project/:projectid",
+  userController.allowIfLoggedin,
+  userController.grantAccess("deleteOwn", "project"),
+  projectController.projectdelete
+);
 
 // router.post('/approve',userController.allowIfLoggedin,submit.request)
 
-router.post('/sendrequest',userController.allowIfLoggedin, submit.request, projectController.sendrequest);
+router.post(
+  "/sendrequest",
+  userController.allowIfLoggedin,
+  submit.request,
+  projectController.sendrequest
+);
 
 // router.put('/response',userController.allowIfLoggedin, projectController.updaterequest);
 
-router.get('/deadline', deadlineController.getdeadlines );
+router.get("/deadline", deadlineController.getdeadlines);
 
-router.post('/deadline', deadlineController.createdeadline);
+router.post("/deadline", deadlineController.createdeadline);
 
-router.get('/user/:userId', userController.allowIfLoggedin, userController.getUser);
- 
-router.get('/users', userController.allowIfLoggedin, userController.grantAccess('readAny', 'profile'), userController.getUsers);
- 
-router.put('/user/:userId', userController.allowIfLoggedin, userController.grantAccess('updateAny', 'profile'), userController.updateUser);
- 
-router.delete('/user/:userId', userController.allowIfLoggedin, userController.grantAccess('deleteAny', 'profile'), userController.deleteUser);
- 
+router.post("/updatedeadlines", deadlineController.updatedeadlines);
+
+router.get(
+  "/user/:userId",
+  userController.allowIfLoggedin,
+  userController.getUser
+);
+
+router.get(
+  "/users",
+  userController.allowIfLoggedin,
+  userController.grantAccess("readAny", "profile"),
+  userController.getUsers
+);
+
+router.put(
+  "/user/:userId",
+  userController.allowIfLoggedin,
+  userController.grantAccess("updateAny", "profile"),
+  userController.updateUser
+);
+
+router.delete(
+  "/user/:userId",
+  userController.allowIfLoggedin,
+  userController.grantAccess("deleteAny", "profile"),
+  userController.deleteUser
+);
+
 module.exports = router;
