8a9845153dda53ef6b649f8ca0e24067da34382b
ShubhamSonawane007 <sksonawane34@gmail.com>
2023-09-20 14:59:06 +0530

    Added login signup rbac system and some changes

--- frontend/src/components/student/StudentProject.js
+++ frontend/src/components/student/StudentProject.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import projects from "../../dummyProj";
 import ProjectCard from "./ProjectCard";
 
 function StudentProject() {
   // Use the filter method to filter projects based on the condition
+
+  const [loggedInUserId, setLoggedInUserId] = useState('');
+
+  useEffect(()=>{
+    fetchUsers();
+},[])
+
+const fetchUsers = () =>{
+    axios
+    .get('http://localhost:3400/signup', {headers:{
+      "x-access-token": localStorage.getItem('token'),
+    },
+  })
+    .then((res) =>{
+      if(res.data.stdid){
+      setLoggedInUserId(res.data.stdid);
+      }
+    })
+}
+
   const filteredProjects = projects.filter(
-    (project) => project.studentid === 1
+    (project) => project.studentid === loggedInUserId
   );
 
   return (
--- frontend/src/dummyProjectAssigned.js
+++ frontend/src/dummyProjectAssigned.js
@@ -43,7 +43,7 @@ let projects = null;
 const obj = {
   headers: {
     "x-access-token":
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NTA4MGM1MjZjZDk1NzE0ODQ0NmI5MGQiLCJpYXQiOjE2OTUxOTc1NDQsImV4cCI6MTY5NTI4Mzk0NH0.3dO5uyMIEgL5hIxgYPkv8YqgUm1VTqcXw8ERgFZU3V8",
+      localStorage.getItem('token'),
   },
 };
 
